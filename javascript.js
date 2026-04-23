d75a13c3848ebd33860a4120c4e553c18f7e7010
DenRavneste <271206167+DenRavneste@users.noreply.github.com>
2026-04-23 13:11:58 +0200

    Set starting variables and start on playRound function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// return one of three variables from function
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * (5 - 1 + 1) + 1);
   if (randomNumber == 1) {
@@ -9,6 +10,15 @@ function getComputerChoice() {
   }
 }
 
+// prompt player for their input
 function getHumanChoice() {
   return prompt("Rock, paper or scissors?");
+}
+
+// set starting values for human and computer score
+let humanScore = 0;
+let computerScore = 0;
+
+// something
+function playRound(humanChoice, computerChoice) {
 }
